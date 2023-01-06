23bf9bd940273399f92c3aa3de6b6739cf36815d
Mikaella Mendoza <122050772+444zip@users.noreply.github.com>
2023-01-06 17:12:07 +0800

    Add files via upload

--- assignment one.java
+++ assignment one.java
@@ -21,6 +21,43 @@ public class App {
             System.out.println("invalid student email address");
         }
 
-        
+         // Java program to check if a phone number is valid
+         Scanner scan = new Scanner(System.in);
+         System.out.print("Enter your phone number: ");
+         String phoneNum = scan.nextLine();
+ 
+      
+         Pattern pattern = Pattern.compile("\\+63\\d{10}");
+         Matcher matcher = pattern.matcher(phoneNum);
+ 
+         boolean match = matcher.matches();
+ 
+          if (match) {
+          System.out.println("valid phone number");
+          } else {
+          System.out.println("invalid phone number");
+      }
+ 
+    
+ 
+ 
+         // Java program to check if a birth date is valid
+         Scanner scan = new Scanner(System.in);
+         System.out.print("What's your birthday? ");
+         String dday = scan.nextLine();
+ 
+         
+         Pattern pattern = Pattern.compile("\\d{4}-\\d{2}-\\d{2}");
+         Matcher matcher = pattern.matcher(dday);
+ 
+         boolean match = matcher.matches();
+ 
+         if (match) {
+             System.out.println("valid birth date");
+         } else {
+             System.out.println("invalid birth date");
+         }
+ 
+ 
         }
     }
