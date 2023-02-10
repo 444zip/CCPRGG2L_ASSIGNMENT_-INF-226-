562fb3e9a58ead3f2adaf032caa58ea95371de8b
Mikaella Mendoza <122050772+444zip@users.noreply.github.com>
2023-02-10 06:23:55 +0000

    Add files via upload

---
+++ exercise five/Daddy.java
@@ -0,0 +1,8 @@
+public class Daddy {
+
+    protected String surname = "Mendoza";
+    
+    protected void showSurname() {
+        System.out.println("My family name is: " + surname );
+    }
+}
---
+++ exercise five/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    private String name = "mika";
+
+    // get() method
+    public String getName(){
+        return this.name;
+    }
+
+    // set() method
+    public void setName(String personName) {
+        this.name = personName;
+    }
+}
